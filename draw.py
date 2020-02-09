8ef70ca30512d343bc422646f4ffaccb9294743c
CalfinChoo <cchu00@stuy.edu>
2020-02-08 22:26:11 -0800

    done

--- draw.py
+++ draw.py
@@ -1,31 +1,99 @@
 from display import *
 
-def draw_line( x0, y0, x1, y1, screen, color ):
-    print(screen)
-    pass
-    deltaX = x1 - x0
-    deltaY = y1 - y0
+def draw_line( x0, y0, x1, y1, screen, color):
+    x0 = int(x0)
+    x1 = int(x1)
+    y0 = int(y0)
+    y1 = int(y1)
+    deltaX = dXY(x0, x1)
+    deltaY = dXY(y0, y1)
     try:
-        m = abs(deltaY) / abs(deltaX)
-    except ZeroDivisionError:
-        for y in range(deltaY):
-            if (y0 > y1): screen[int(y1 + y)][int(x0)] = color
-            else: screen[int(y0 + y)][int(x0)] = color
+        m = deltaY / deltaX
+    except ZeroDivisionError: #vertical
+        for y in range(abs(deltaY)):
+            if (y0 > y1): screen[y1 + y][x0] = color.copy()
+            else: screen[y0 + y][x0] = color.copy()
         return
-    #d = 2A + B
-    #A = deltaY
-    #B = -deltaX
-    # if (m > 0 and m < 1):
-    #     d = func(dXY(y0, y1), -1 * dXY(x0, x1))
-    #     while (x0 <= x1):
-    #         screen[x0][y0] = color
-    #         if (d > 0):
-    #             y0 = y0 + 1
-    #             d =  d + -2 * dXY(x0, x1)
-    #         x0 = x0 + 1
+    if (m == 0): #horizontal
+        for x in range(abs(deltaX)):
+            if (x0 > x1): screen[y0][x1 + x] = color.copy()
+            else: screen[y0][x0 + x] = color.copy()
+    if (m > 0 and m <= 1): #octant 1 + 5
+        d = func(dXY(y0, y1), -1 * dXY(x0, x1))
+        if (x0 < x1):
+            while (x0 < x1):
+                screen[y0][x0] = color.copy()
+                if (d > 0):
+                    y0 = y0 + 1
+                    d =  d + -2 * dXY(x0, x1)
+                x0 = x0 + 1
+                d = d + 2 * dXY(y0, y1)
+        else:
+            while (x0 > x1):
+                screen[y0][x0] = color.copy()
+                if (d > 0):
+                    y0 = y0 - 1
+                    d =  d + -2 * dXY(x1, x0)
+                x0 = x0 - 1
+                d = d + 2 * dXY(y1, y0)
+    if (m > 1): #octants 2 + 6
+        d = func(-1 * dXY(x0, x1), dXY(y0, y1))
+        if (x0 < x1):
+            while (x0 < x1):
+                screen[y0][x0] = color.copy()
+                if (d < 0):
+                    x0 = x0 + 1
+                    d = d + 2 * dXY(y0, y1)
+                y0 = y0 + 1
+                d =  d + -2 * dXY(x0, x1)
+        else:
+            while (x0 > x1):
+                screen[y0][x0] = color.copy()
+                if (d < 0):
+                    x0 = x0 - 1
+                    d =  d + 2 * dXY(y1, y0)
+                y0 = y0 - 1
+                d = d + -2 * dXY(x1, x0)
+    if (m < -1): #octants 3 + 7
+        d = func(-1 * dXY(x0, x1), dXY(y0, y1))
+        if (x0 < x1):
+            while (x0 < x1):
+                screen[y0][x0] = color.copy()
+                if (d < 0):
+                    x0 = x0 + 1
+                    d =  d + -2 * dXY(y0, y1)
+                y0 = y0 - 1
+                d = d - 2 * dXY(x0, x1)
+        else:
+            while (x0 > x1):
+                screen[y0][x0] = color.copy()
+                if (d > 0):
+                    x0 = x0 - 1
+                    d = d + 2 * dXY(y1, y0)
+                y0 = y0 + 1
+                d =  d + 2 * dXY(x1, x0)
+    if (m < 0 and m >= -1): #octants 4 + 8
+        d = func(dXY(y0, y1), -1 * dXY(x0, x1))
+        if (x0 < x1):
+            while (x0 < x1):
+                screen[y0][x0] = color.copy()
+                if (d > 0):
+                    y0 = y0 - 1
+                    d =  d + -2 * dXY(x0, x1)
+                x0 = x0 + 1
+                d = d - 2 * dXY(y0, y1)
+        else:
+            while (x0 > x1):
+                screen[y0][x0] = color.copy()
+                if (d < 0):
+                    y0 = y0 + 1
+                    d =  d + 2 * dXY(x1, x0)
+                x0 = x0 - 1
+                d = d + 2 * dXY(y1, y0)
+
 
 def dXY(xy0, xy1):
-    return xy1 - xy0
+    return int(xy1 - xy0)
 
 def func(a, b):
     d = 2 * a + b
--- main.py
+++ main.py
@@ -2,36 +2,119 @@ from display import *
 from draw import *
 
 s = new_screen()
-c = [ 0, 255, 0 ]
+c = [ 255, 255, 255 ]
 
-#octants 1 and 5
-draw_line(0, 0, XRES-1, YRES-1, s, c)
-draw_line(0, 0, XRES-1, YRES / 2, s, c)
-draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c)
+draw_line(XRES/4, YRES/3, XRES/4 + 10, YRES/3, s, c)
+draw_line(XRES/4, YRES/3, XRES/4 - 10, YRES/3, s, c)
 
-#octants 8 and 4
-c[BLUE] = 255;
-draw_line(0, YRES-1, XRES-1, 0, s, c);
-draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
-draw_line(XRES-1, 0, 0, YRES/2, s, c);
+draw_line(XRES/4 + 10, YRES/3, XRES/4 + 10, YRES/3+10, s, c)
+draw_line(XRES/4 - 10, YRES/3, XRES/4 - 10, YRES/3+10, s, c)
 
-#octants 2 and 6
-c[RED] = 255;
-c[GREEN] = 0;
-c[BLUE] = 0;
-draw_line(0, 0, XRES/2, YRES-1, s, c);
-draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
+draw_line(XRES/4 + 10, YRES/3+10, XRES/4 + 25, YRES/3+10, s, c)
+draw_line(XRES/4 - 10, YRES/3+10, XRES/4 - 25, YRES/3+10, s, c)
 
-#octants 7 and 3
-c[BLUE] = 255;
-draw_line(0, YRES-1, XRES/2, 0, s, c);
-draw_line(XRES-1, 0, XRES/2, YRES-1, s, c);
+draw_line(XRES/4 + 25, YRES/3+10, XRES/4 + 25, YRES/3, s, c)
+draw_line(XRES/4 - 25, YRES/3+10, XRES/4 - 25, YRES/3, s, c)
 
-#horizontal and vertical
-c[BLUE] = 0;
-c[GREEN] = 255;
-draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
-draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
+draw_line(XRES/4 + 25, YRES/3, XRES/4 + 40, YRES/3, s, c)
+draw_line(XRES/4 - 25, YRES/3, XRES/4 - 40, YRES/3, s, c)
+
+draw_line(XRES/4 + 40, YRES/3, XRES/4 + 40, YRES/3+25, s, c)
+draw_line(XRES/4 - 40, YRES/3, XRES/4 - 40, YRES/3+25, s, c)
+draw_line(XRES/4 - 40, YRES/3+25, XRES/4 + 40, YRES/3+25, s, c)
+
+draw_line(XRES/4 - 35, YRES/3+30, XRES/4 - 35, YRES/3+25, s, c)
+draw_line(XRES/4 + 35, YRES/3+30, XRES/4 + 35, YRES/3+25, s, c)
+draw_line(XRES/4 - 35, YRES/3+30, XRES/4 + 35, YRES/3+30, s, c)
+
+draw_line(XRES/4 - 30, YRES/3+40, XRES/4 - 30, YRES/3+30, s, c)
+draw_line(XRES/4 + 30, YRES/3+40, XRES/4 + 30, YRES/3+30, s, c)
+draw_line(XRES/4 - 30, YRES/3+40, XRES/4 + 30, YRES/3+40, s, c)
+
+draw_line(XRES/4 - 23, YRES/3+40, XRES/4 - 32, YRES/3+150, s, c)
+draw_line(XRES/4 + 23, YRES/3+40, XRES/4 + 32, YRES/3+150, s, c)
+draw_line(XRES/4 - 32, YRES/3+150, XRES/4 + 32, YRES/3+150, s, c)
+
+draw_line(XRES/4 - 35, YRES/3+150, XRES/4 - 35, YRES/3+155, s, c)
+draw_line(XRES/4 + 35, YRES/3+150, XRES/4 + 35, YRES/3+155, s, c)
+draw_line(XRES/4 - 35, YRES/3+150, XRES/4 + 35, YRES/3+150, s, c)
+draw_line(XRES/4 - 35, YRES/3+155, XRES/4 + 35, YRES/3+155, s, c)
+
+draw_line(XRES/4 - 40, YRES/3+165, XRES/4 - 40, YRES/3+155, s, c)
+draw_line(XRES/4 + 40, YRES/3+165, XRES/4 + 40, YRES/3+155, s, c)
+draw_line(XRES/4 - 40, YRES/3+155, XRES/4 + 40, YRES/3+155, s, c)
+draw_line(XRES/4 - 40, YRES/3+165, XRES/4 + 40, YRES/3+165, s, c)
+
+draw_line(XRES/4 - 55, YRES/3+185, XRES/4 - 50, YRES/3+165, s, c)
+draw_line(XRES/4 + 55, YRES/3+185, XRES/4 + 50, YRES/3+165, s, c)
+draw_line(XRES/4 - 50, YRES/3+165, XRES/4 + 50, YRES/3+165, s, c)
+draw_line(XRES/4 - 55, YRES/3+185, XRES/4 + 55, YRES/3+185, s, c)
+
+draw_line(XRES/4 - 57, YRES/3+195, XRES/4 - 57, YRES/3+185, s, c)
+draw_line(XRES/4 + 57, YRES/3+195, XRES/4 + 57, YRES/3+185, s, c)
+draw_line(XRES/4 - 57, YRES/3+185, XRES/4 + 57, YRES/3+185, s, c)
+draw_line(XRES/4 - 57, YRES/3+195, XRES/4 + 57, YRES/3+195, s, c)
+
+draw_line(XRES/4 - 55, YRES/3+195, XRES/4 - 55, YRES/3+198, s, c)
+draw_line(XRES/4 + 55, YRES/3+195, XRES/4 + 55, YRES/3+198, s, c)
+draw_line(XRES/4 - 55, YRES/3+198, XRES/4 + 55, YRES/3+198, s, c)
+#-----------------------------
+c = [ 128, 128, 128]
+
+draw_line(XRES*3/4, YRES/3, XRES*3/4 + 10, YRES/3, s, c)
+draw_line(XRES*3/4, YRES/3, XRES*3/4 - 10, YRES/3, s, c)
+
+draw_line(XRES*3/4 + 10, YRES/3, XRES*3/4 + 10, YRES/3+10, s, c)
+draw_line(XRES*3/4 - 10, YRES/3, XRES*3/4 - 10, YRES/3+10, s, c)
+
+draw_line(XRES*3/4 + 10, YRES/3+10, XRES*3/4 + 25, YRES/3+10, s, c)
+draw_line(XRES*3/4 - 10, YRES/3+10, XRES*3/4 - 25, YRES/3+10, s, c)
+
+draw_line(XRES*3/4 + 25, YRES/3+10, XRES*3/4 + 25, YRES/3, s, c)
+draw_line(XRES*3/4 - 25, YRES/3+10, XRES*3/4 - 25, YRES/3, s, c)
+
+draw_line(XRES*3/4 + 25, YRES/3, XRES*3/4 + 40, YRES/3, s, c)
+draw_line(XRES*3/4 - 25, YRES/3, XRES*3/4 - 40, YRES/3, s, c)
+
+draw_line(XRES*3/4 + 40, YRES/3, XRES*3/4 + 40, YRES/3+25, s, c)
+draw_line(XRES*3/4 - 40, YRES/3, XRES*3/4 - 40, YRES/3+25, s, c)
+draw_line(XRES*3/4 - 40, YRES/3+25, XRES*3/4 + 40, YRES/3+25, s, c)
+
+draw_line(XRES*3/4 - 35, YRES/3+30, XRES*3/4 - 35, YRES/3+25, s, c)
+draw_line(XRES*3/4 + 35, YRES/3+30, XRES*3/4 + 35, YRES/3+25, s, c)
+draw_line(XRES*3/4 - 35, YRES/3+30, XRES*3/4 + 35, YRES/3+30, s, c)
+
+draw_line(XRES*3/4 - 30, YRES/3+40, XRES*3/4 - 30, YRES/3+30, s, c)
+draw_line(XRES*3/4 + 30, YRES/3+40, XRES*3/4 + 30, YRES/3+30, s, c)
+draw_line(XRES*3/4 - 30, YRES/3+40, XRES*3/4 + 30, YRES/3+40, s, c)
+
+draw_line(XRES*3/4 - 23, YRES/3+40, XRES*3/4 - 32, YRES/3+150, s, c)
+draw_line(XRES*3/4 + 23, YRES/3+40, XRES*3/4 + 32, YRES/3+150, s, c)
+draw_line(XRES*3/4 - 32, YRES/3+150, XRES*3/4 + 32, YRES/3+150, s, c)
+
+draw_line(XRES*3/4 - 35, YRES/3+150, XRES*3/4 - 35, YRES/3+155, s, c)
+draw_line(XRES*3/4 + 35, YRES/3+150, XRES*3/4 + 35, YRES/3+155, s, c)
+draw_line(XRES*3/4 - 35, YRES/3+150, XRES*3/4 + 35, YRES/3+150, s, c)
+draw_line(XRES*3/4 - 35, YRES/3+155, XRES*3/4 + 35, YRES/3+155, s, c)
+
+draw_line(XRES*3/4 - 40, YRES/3+165, XRES*3/4 - 40, YRES/3+155, s, c)
+draw_line(XRES*3/4 + 40, YRES/3+165, XRES*3/4 + 40, YRES/3+155, s, c)
+draw_line(XRES*3/4 - 40, YRES/3+155, XRES*3/4 + 40, YRES/3+155, s, c)
+draw_line(XRES*3/4 - 40, YRES/3+165, XRES*3/4 + 40, YRES/3+165, s, c)
+
+draw_line(XRES*3/4 - 55, YRES/3+185, XRES*3/4 - 50, YRES/3+165, s, c)
+draw_line(XRES*3/4 + 55, YRES/3+185, XRES*3/4 + 50, YRES/3+165, s, c)
+draw_line(XRES*3/4 - 50, YRES/3+165, XRES*3/4 + 50, YRES/3+165, s, c)
+draw_line(XRES*3/4 - 55, YRES/3+185, XRES*3/4 + 55, YRES/3+185, s, c)
+
+draw_line(XRES*3/4 - 57, YRES/3+195, XRES*3/4 - 57, YRES/3+185, s, c)
+draw_line(XRES*3/4 + 57, YRES/3+195, XRES*3/4 + 57, YRES/3+185, s, c)
+draw_line(XRES*3/4 - 57, YRES/3+185, XRES*3/4 + 57, YRES/3+185, s, c)
+draw_line(XRES*3/4 - 57, YRES/3+195, XRES*3/4 + 57, YRES/3+195, s, c)
+
+draw_line(XRES*3/4 - 55, YRES/3+195, XRES*3/4 - 55, YRES/3+198, s, c)
+draw_line(XRES*3/4 + 55, YRES/3+195, XRES*3/4 + 55, YRES/3+198, s, c)
+draw_line(XRES*3/4 - 55, YRES/3+198, XRES*3/4 + 55, YRES/3+198, s, c)
 
 
 display(s)
